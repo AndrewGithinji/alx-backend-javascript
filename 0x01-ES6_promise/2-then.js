4b788985f3bce6f1f66046241a7ac8ed33077e47
AndrewGithinji <andrewgithinji@hotmail.com>
2023-04-20 13:40:49 +0300

    Got a response from the API

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,13 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    promise
+      .then(() => {
+        console.log('Got a response from the API');
+        resolve({ status: 200, body: 'success' });
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(new Error());
+      });
+  });
+}
