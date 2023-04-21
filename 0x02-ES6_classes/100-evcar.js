7754c76573ce5f50481f2cfeba0dac0e6b884524
AndrewGithinji <andrewgithinji@hotmail.com>
2023-04-21 21:42:31 +0300

    Implement a class named EVCar that extends the Car

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,28 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
